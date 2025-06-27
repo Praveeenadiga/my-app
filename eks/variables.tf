bea61ee7a31106cf51287d2654c7c626be820dfe
PRAVEEN KUMAR ADIGA <praveenkumaradiga@PRAVEENs-MacBook-Pro.local>
2025-06-27 19:31:12 +0530

    teating

---
+++ eks/variables.tf
@@ -0,0 +1,16 @@
+variable "project" {
+  type = string
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "node_policies" {
+  type    = list(string)
+  default = [
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  ]
+}
