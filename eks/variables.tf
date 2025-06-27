348bd7c7b6341e1322dfd67f22df5b123d3aa3fa
PRAVEEN KUMAR ADIGA <praveenkumaradiga@PRAVEENs-MacBook-Pro.local>
2025-06-28 01:51:07 +0530

    testing

--- eks/variables.tf
+++ eks/variables.tf
@@ -14,19 +14,4 @@ variable "node_policies" {
     "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   ]
 }
-variable "project" {
-  type = string
-}
 
-variable "private_subnets" {
-  type = list(string)
-}
-
-variable "node_policies" {
-  type    = list(string)
-  default = [
-    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  ]
-}
---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source               = "./vpc"
+  aws_region           = var.aws_region
+  project              = var.project
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+}
+
+module "eks" {
+  source          = "./eks"
+  project         = var.project
+  private_subnets = module.vpc.private_subnets
+}
