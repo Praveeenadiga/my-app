be57979b6ef19770c236a970ce8d290ecdd93746
PRAVEEN KUMAR ADIGA <praveenkumaradiga@PRAVEENs-MacBook-Pro.local>
2025-06-28 19:52:19 +0530

    testing

--- backend.tf
+++ backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "adigaokpkyake1234"   # Replace with your bucket name
-    key            = "eks-cluster/terraform.tfstate" # Path inside bucket
-    region         = "us-east-1"
-             # Optional, for locking
-    encrypt        = true
+    bucket  = "adigaokpkyake1234"                 # Replace with your bucket name
+    key     = "eks-cluster/terraform.tfstate"     # Path inside bucket
+    region  = "us-east-1"
+    encrypt = true
   }
 }
